25d1cf8c1657115476a9903a0ba8b6d6421b4058
Isabel Contreras <isabel.contreras.1999@hotmail.com>
2021-03-13 23:54:14 -0600

    Adición de código respectivo a: nombre de ruta y combinar rutas de atributo.

--- Controllers/MyProductsController.cs
+++ Controllers/MyProductsController.cs
@@ -16,13 +16,56 @@ namespace MvcMovie.Controllers
         [HttpGet("/products3")]
         public IActionResult ListProducts()
         {
-            return View("../Movies/Index");
+            return new StatusCodeResult(400);
         }
 
         [HttpPost("/products3")]
         public IActionResult CreateProduct(Movie myProduct)
         {
-            return View("../Movies/Create"); 
+            return new StatusCodeResult(500);
+        }
+    }
+    [ApiController]
+    public class Products2ApiController : ControllerBase
+    {
+        [HttpGet("/products2/{id}", Name = "Products_List")]
+        public IActionResult GetProduct(int id)
+        {
+            return new StatusCodeResult(404);
+        }
+    }
+    [ApiController]
+    [Route("products")]
+    public class ProductsApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ListProducts()
+        {
+            return new StatusCodeResult(206);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return new StatusCodeResult(303);
+        }
+    }
+
+    [Route("Home2")]
+    public class Home2Controller : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return new StatusCodeResult(307);
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return new StatusCodeResult(308);
         }
     }
 }
